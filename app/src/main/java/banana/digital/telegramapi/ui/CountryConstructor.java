480c78d1d85b1ec4acbbfdc45781f9fb68e8b776
tiget <tigetxd@gmail.com>
2018-12-03 17:44:36 +0300

    yes

---
+++ app/src/main/java/banana/digital/telegramapi/ui/CountryConstructor.java
@@ -0,0 +1,26 @@
+package banana.digital.telegramapi.ui;
+
+import java.io.Serializable;
+
+public class CountryConstructor implements Serializable {
+    String CountryName;
+    int CountryCode;
+
+
+    public CountryConstructor(String CountryName, int CountryCode){
+        this.CountryName = CountryName;
+        this.CountryCode = CountryCode;
+
+    }
+
+
+    public String getCategoryName(){
+        return CountryName;
+    }
+
+    public int getCategoryBackground(){
+        return CountryCode;
+    }
+
+
+}
